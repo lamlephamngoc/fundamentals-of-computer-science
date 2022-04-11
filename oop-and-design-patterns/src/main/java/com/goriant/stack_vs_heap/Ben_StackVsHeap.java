a6c30b88e2ce4d5309ab9210999dd781301a6755
Lam Le <lamle@gmx.com>
2022-04-11 11:21:47 +0700

    stack vs heap

--- oop-and-design-patterns/src/main/java/com/goriant/stack_vs_heap/Ben_StackVsHeap.java
+++ oop-and-design-patterns/src/main/java/com/goriant/stack_vs_heap/Ben_StackVsHeap.java
@@ -2,99 +2,55 @@ package com.goriant.stack_vs_heap;
 
 public class Ben_StackVsHeap {
 
-    /*
-        - Fundamentals of JVM
-        - memory: java memory model (JMM)
-        - Byte code: JVM is cross OS
-        - Class Loader:
-        - CPU/RAM/DISK:
-            - CPU: execute byte code instruction
-            - RAM (Random access memory): store data state
-            - DISK:
-                - JAVA source code
-                - Java byte code
-
-        - Stack and Heap:
-            - Heap: store data state
-            - Stack: store byte code instruction
-                - Thread:
-                    - each thread has a seperated stack
-                    - CPU: sẻ đọc cái stack của cái thread line by line and execute
-                        - Main Thead STACK of cái Class này:
-                            - nó đã dc push/put line code của cái Class này vào
-                            - nó run line by line từ trên xuống
-
-                            Lucas's statement:
-                                - từ Perm gen: JVM copy class Ben_StackVsHeap metadata vào Main Thread Stack và run line by line
-                                - Stack - Main Thread Stack - ThreadLocal : CPU sẽ đọc instruction từ Thread Stack và evaluate line by line (anh ko quan tâm là FIFO, LIFO)
-
-
-        - JVM architecture nó sẻ có 3 thành phần chính
-        - Class Loader
-        - Data Area
-        - Execution Engine
-
-        1. how does jvm execute line by line
-        - JVM's compiler nó sẻ load những cái file .java thành file .class
-        - JVM's ClassLoader nó sẻ convert file .class thành byte code lên vùng nhớ permgem
-        - JVM's Exution Engine thằng này nó sẻ convert thanh Languge Machine instruction
-        - CPU sẻ execute mấy cái byte code dựa trên instruction
-
-        - đầu tiên khi hàm main chạy lên sẻ execute line code Person person = new Person();
-        -  khi một thread mainc hạy lên sẻ được JVM cung cấp cho 1 cái stack
-        - biến person sẻ được lưu ở trong stack chứa địa chỉ vùng nhớ trỏ tới object "new Person()"
-        - "new Person()" nó sẻ tạo 1 vùng ở trong Heap để lưu data
-        - vì Class Person có 1 field là "name" với dataType la String,
-         thì tại cái vùng nhớ "new Person()" field "name" nó sẻ trỏ tới null vì chưa có giá trị được gán.
-
-         - đến line code person.setName("Ben");
-         - biến person vẫn được lưu ở trong stack và vẫn trỏ tới vùng nhớ ban đầu
-         - "Ben" nó là 1 cái literal String và khi mình khai báo "Ben" là nó tạo 1 vùng nhớ ở String Pool để chứa giá trị này
-         - và nó là immutable mình sẻ discuss cái này sau vì không nằm trong session này
-         - field "name" trong object person sẻ đổi từ null sang vùng nhớ của "Ben" trong heap.
-
-         - đến line code "person.setName("Lucas");" thì cũng tương tự như line code "person.setName("Ben");"
-         - field "name" chỉ đổi địa chỉ vùng nhớ.
-
-         - Call/ Thread Stack
-         - một cái thread main khi được tạo, JVM sẻ cấp cho nó 1 vùng nhớ Stack, và cái này là độc lập giữa các thread main.
-         -
-         - Stack: Last In First Out
-         - function sẻ được bỏ vào stack khi mà nó gọi execute
-         - vì sao nó lại bỏ vào stack:
-         - thì để cái thằng Execution Engine, nó đánh dấu lại cái vị trí khi gọi execute cái function đó
-         */
-
-    /*
-    STACK
-    *******
-     */
     public static void main(String[] args) {
-        Person person = new Person();
-        person.setName("Ben");
-        person.setName("Lucas");
+        Person p = createPerson();
+        System.out.println(p);
+    }
+    public static Person createPerson() {
+//        for(StackTraceElement e: Thread.currentThread().getStackTrace())
+//            System.out.println(e);
+        Person p = new Person();
+        p.name = "Ben";
+        p.name = "lucas";
+        return p;
     }
 
     public static class Person {
-        private String name;
+        public String name;
 
-        public Person() {
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
         }
-
-        public void setName(String name) {
-            this.name = name;
-        }
     }
 
-    static void methodA() {
+    // Problems solving skills
+    //  - ko chịu do homework/R&D
+    //  - stackoverflow copy paste
+    //  - baeldung.com copy paste - Ben nói là resouce dc verified by ? - Ben sẽ show cho anh :D
 
-    }
+    /*
+    - Stackoverflow: tràn vùng nhớ Stack trong JVM
+        - thread stack memory -> OS - JDK flags
+        - complete a Block Code - a Function
+        - đệ quy ko có break point: nó sẽ put a function to its Stack
+            - nó cứ put cho đến khi tràn Stack Memory -> application crash by ERROR: java.lang.StackOverflowError
+    - normal:
+        - nó sẽ put từng blockcode vào Thread Stack và evaluate
+            - Blockcode của mình ở đâu ra cho Thread Stack
+                - 1. Execution Engine: convert bytecode thành Language Machine Instruction (CPU run cái lày) : run line by line
+                - 2. Execution Engine: execute blockcode of function dc put vào stack đó
+                - 3. how?
+                    - STACK: 1 cái kệ
+                        - Execution Engine: sẽ pop từng line ra và execute
+                        - Sau khi nó POP từ Stack ra - thì nó sẽ vứt cái line này trong Stack của nó đi
+                            1. là nó sẽ clear cái Thread Stack data của nó
+                            2. là GC sẽ lắng nghe và tất cả Local Variable trong blockcode này - sẽ dc dọn gần như ngay lập tức
+                                - avoid Stackoverflow - Stack Memory
+                                - GC có info để dọn dẹp Heap Memory cho mình
 
-    static void methodB() {
-
-    }
-
-    static void methodC() {
-
-    }
+    - NOTES: ngoại trừ docs của Oracle JAVA tất cả đều ko dc verified
+    */
 }
